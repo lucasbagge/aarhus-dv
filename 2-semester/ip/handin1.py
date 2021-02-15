29e155415b6fac5334efd36f6644606249e3df60
lucas bagge <lucasbagge@lucass-MacBook-Pro.local>
2021-02-15 15:04:39 +0100

    correctnes.

--- 2-semester/ip/handin1.py
+++ 2-semester/ip/handin1.py
@@ -7,9 +7,14 @@ This handin is done by (study ids and names of up to three students):
 
 Reflection upon solution:
 
-    <Jeg skulle afleverer opgaven igen.
-    I den forbindelse har jeg rettet min koden på bedste vis ud fra rettelser, hvor jeg har
-    erstattet for loop med et while loop, fjerner pandas og numpy>
+    <
+    I denne genaflevering har jeg gjort op med de sidste nødvendig rettelser.
+    Jeg har rettet i min beregning af renten og sørgede for at lånet går i nul.
+    Desuden skulle jeg checke om lånet var muligt. For at tilføje det har jeg inkluderet
+    en if statment i mit while loop. Jeg blev oprindelig bedt om det før, men da jeg
+    definerer `calculation` så havde jeg svært at se hvordan jeg kunne gøre det før, 
+    men at have den med tænker jeg giver samme funktionalitet. 
+    >
 """
 
 P = float(input("Loan amount \n"))
@@ -17,7 +22,7 @@ rent = float(input("Your interest rent \n"))
 mp = float(input('Your monthly payment \n'))
 
 def calculation(P, mp, interest_rate = rent):
-    interest_paid = (((rent/12) * P) * 100) / 100
+    interest_paid = rent * P
     principal_paid = round(mp - interest_paid, 2)
     new_balance = round(P - principal_paid, 2)
     return(mp, interest_paid, principal_paid, new_balance)
@@ -27,10 +32,13 @@ count = 0
 
 while P > 0:
     mp,i_paid,p_paid,new_p = calculation(P, mp)
+    if (new_p > P): 
+        print("Not possible")
+        break 
     count += 1
-    if (new_p < 0): break
-    payment_list.append([count, new_p])
+    if (new_p < 0): 
+        new_p = 0
     P = round(new_p, 2)
-print(payment_list)
+    print("month number :", count, "remaining debt:", new_p)
 ...
 
