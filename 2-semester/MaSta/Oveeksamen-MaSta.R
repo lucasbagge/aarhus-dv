fcc672bd9c847829b71037c8b30786e56b273912
lucas bagge <lucasbagge@lucass-MacBook-Pro.local>
2021-03-17 19:49:02 +0100

    ms aflevering

---
+++ 2-semester/MaSta/Oveeksamen-MaSta.R
@@ -0,0 +1,40 @@
+#' ---
+#' title: "Øveeksamen Matematisk Statistik"
+#' author: "Angiv dit navn og studienummer her"
+#' date: "her står eksamensdato"
+#' output:
+#'   pdf_document: default
+#'   html_document: default
+#'   word_document: default
+#' documentclass: article
+#' classoption: a4paper
+#' ---
+#' 
+#' Til eksamen vil I få en skabelon magen til den her, dog muligvis uden lange overskrifter til delopgaverne. Husk at lade en tom linie stå før hver overskrift, ellers virker det ikke.
+#' 
+#' 
+#' ## Opgave 1
+#' 
+#' ### (1.a) Boxplots
+#' 
+#' 
+#' ### (1.b) Permutationstest
+#' 
+#' 
+#' ### (1.c) Grafisk vurdering af normalfordelingen
+#' 
+#' 
+#' ### (1.d) Konfidensgrænse
+#' 
+#' 
+#' ************
+#' 
+#' ## Opgave 2
+#' 
+#' ### (2.a) Likelihood
+#' 
+#' ### (2.b) Maksimum-likelihood estimator
+#' 
+#' ### (2.c) Fordeling og konfidensinterval
+#' 
+#' 
