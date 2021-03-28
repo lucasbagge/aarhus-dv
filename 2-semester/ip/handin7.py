9112a08394cd364c0dd399b77b27154eb29fd4bd
lucas bagge <lucasbagge@lucass-MacBook-Pro.local>
2021-03-28 21:36:25 +0200

    handin7 rettelser.

--- 2-semester/ip/handin7.py
+++ 2-semester/ip/handin7.py
@@ -8,17 +8,9 @@ This handin is done by (study ids and names of up to three students):
 Reflection upon solution:
 
     <
-	I denne uge har vi skulle implementer en algoritme til kaldet Convex hull.
-	Jeg er kommet frem til en løsning til opgave a, c og d. I opgave c har jeg taget brug af at beregne øvre og nedre
-	dele og herefter samle dem.
-	I opgave d skulle vi implemterer nogle test af vores funktion i form af assert og doctest. Jeg har delt min funktion fra c
-	op i to dele, hvor i den ene benytter jeg nogle simple assert statement for at se om min funktion virke. For at benytte doctest
-	har jeg givet et eksempel på hvordan jeg regne mit funktion conv_hull vil være. 
-	Jeg har nogle mangle, som jeg gerne vil have feedback på:
-	* i opgave b) kunne jeg ikke finde ud af at bruge hintene til at lave en plot_hull funktion. Jeg har svært ved at se hvordan det skal
-	gøres?
-	* Jeg kunne måske i d gøre mere ud af at lave flere test af min funktion?
-	* Jeg bruger at array til at plotte funktionen, er det ok eller en dum løsning?
+    Jeg har kigget på rettelser og fjeret i min convex_hull at den giver mig en array
+    I b har jeg tilføjet en plt_hull funktion. Den giver mig et plot som jeg er okay tilfreds med,
+    men den er ikke fuldstændig en til en med Gerths plot.  
     >
 """
 # Moduler ----
@@ -39,20 +31,25 @@ def random_points(n):
   x_coordinate = [0.99 if x == 1 else x for x in x_coordinate]
   y_coordinate = [0.99 if x == 1 else x for x in y_coordinate]
   return [(x,y) for x,y in zip(x_coordinate, y_coordinate)]
-
 print("Mine random tal par: \n", random_points(5))
 
+# lternativ og mere fimpel
+
+def random_points2(n):
+    return [(ra.random(),ra.random()) for i in range(n)]
+print("Mine random tal par: \n", random_points2(5))
+
 # b) ----
-
-#def plot_hull(points, polygon):
-#    polygon = np.array(polygon)
-#
-#	fig = plt.figure
-#	plt.plot(points[:,0],points[:,1], 'r-')
-#	plt.plot([points[-1,0],points[0,0]],[points[-1,1],points[0,1]], 'b-')
-#	plt.plot(polygon[:,0],polygon[:,1],"go")
-#	return fig
-
+def plot_hull(points, polygon):
+  link_hole_plot = list(points[i] for i in [0,-1])
+  x0,y0  = list(zip(*link_hole_plot))
+  x1,y1  = list(zip(*points))
+  x2,y2  = list(zip(*polygon))
+  plt.plot(x0, y0, "r-")
+  plt.plot(x1, y1, "r-")
+  plt.plot(x2, y2, "go")
+  plt.show()
+  
 # c) ----
 # Jeg kunne ikke få left_turn til at virke, men gjorde jeg det med right turn ville det virke.
 def right_turn(p1, p2, p3):
@@ -78,16 +75,13 @@ def convex_hull(P):
 	del L_lower[0]
 	del L_lower[-1]
 	L = L_upper + L_lower # Bygger hele stykket.
-	return np.array(L)
+	return list(L)
 
-P = random_points(10)
-L = convex_hull(P)
-print("convex hull: \n", L)
-P = np.array(P)
-plt.gca()
-plt.plot(L[:,0],L[:,1], 'r-')
-plt.plot([L[-1,0],L[0,0]],[L[-1,1],L[0,1]], 'b-')
-plt.plot(P[:,0],P[:,1],"go")
+# plot
+points = random_points(1000)
+test = convex_hull(points)
+plt.clf()
+plot_hull(test, points)
 
 # d) ----
 ## assert
@@ -119,9 +113,8 @@ assert (L.ndim == 2)
   def convex_hull(P):
       '''
   >>> convex_hull([(0.5,2),(1,1), (2,3)])
-  array([[0.5, 2. ],
-       [1. , 1. ],
-       [2. , 3. ]])
+  [(0.5, 2), (1, 1), (2, 3)]
+
   '''
 	P.sort() # Sort the set of points
 	L_upper = [P[0], P[1]] # Initialize upper part
