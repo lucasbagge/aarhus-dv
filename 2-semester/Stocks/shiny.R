41c0db97f5f1c608a8eefa7a40ae359ca082b9b4
lucas bagge <lucasbagge@lucass-MacBook-Pro.local>
2021-04-07 23:09:36 +0200

    shiny

---
+++ 2-semester/Stocks/shiny.R
@@ -0,0 +1,104 @@
+## app.R ##
+library(shiny)
+library(shinydashboard)
+library(quantmod)
+library(highcharter)
+library(dplyr)
+library(magrittr)
+library(tidyquant)
+library(ggplot2)
+library(plotly)
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody(
+    fluidRow(
+      box(highchartOutput("plot1"), width = "100%", height = "100%"),
+      box(plotOutput("plot2")))
+    
+  )
+)
+
+server <- function(input, output) { 
+  SPY_tibble <- tq_get("ORSTED.CO", get="stock.prices")
+  SPY_tibble_1 <- 
+    SPY_tibble %>% 
+    tq_mutate(select = c(close), mutate_fun = SMA, n = 12) %>% 
+    rename(SMA_12 = SMA) %>% 
+    tq_mutate(select = c(close), mutate_fun = SMA, n = 50) %>% 
+    mutate(sma5tr_tibble = Lag(case_when(
+      Lag(close) < Lag(SMA) & close > SMA ~ 1,
+      Lag(close) > Lag(SMA) & close < SMA ~ -1,
+      TRUE ~ 0))) %>% 
+    tq_mutate(select     = close, 
+              mutate_fun = MACD, 
+              nFast      = 12, 
+              nSlow      = 26, 
+              nSig       = 9, 
+              maType     = SMA) %>%
+    mutate(diff = macd - signal)
+  
+  SPY_tibble_1 %>%
+    filter(date > "2020-06-01") %>% 
+    ggplot(aes(x = date, y = close, open = open,
+               high = high, low = low, close = close)) +
+    geom_candlestick() +
+    geom_bbands(ma_fun = SMA, sd = 2, n = 20, 
+                linetype = 4, size = 1, alpha = 0.2, 
+                fill        = palette_light()[[1]], 
+                color_bands = palette_light()[[1]], 
+                color_ma    = palette_light()[[2]]) +
+    labs(title = "AAPL Candlestick Chart", 
+         subtitle = "BBands with SMA Applied, Experimenting with Formatting", 
+         y = "Closing Price", x = "")  +
+    theme_tq()
+  
+  SPY_tibble_1 %>%
+    filter(date > "2020-06-01") %>% 
+    ggplot(aes(x = date)) + 
+    geom_hline(yintercept = 0, color = palette_light()[[1]]) +
+    geom_line(aes(y = macd)) +
+    geom_line(aes(y = signal), color = "blue", linetype = 2) +
+    geom_bar(aes(y = diff), stat = "identity", color = palette_light()[[1]]) +
+    labs(title = "FANG: Moving Average Convergence Divergence",
+         y = "MACD", x = "", color = "") +
+    theme_tq() +
+    scale_color_tq()
+  
+  output$plot1 <- renderHighchart({
+    highchart(type = "stock") %>% 
+      # hc_yAxis_multiples(
+      #   create_yaxis(2, height = c(2, 1), turnopposite = TRUE)
+      # ) %>% 
+      hc_add_series(SPY_tibble_1 %>% timetk::tk_xts(date_var = "date"), 
+                    #yAxis = 0,
+                    name = "SPY")  %>% 
+      hc_add_series(SPY_tibble_1 %>% 
+                      select(date, SMA) %>% 
+                      timetk::tk_xts(date_var = "date"),
+                    #yAxis = 0, 
+                    name = "SMA - 50") %>% 
+      hc_add_series(SPY_tibble_1 %>% 
+                      select(date, SMA_12) %>% 
+                      timetk::tk_xts(date_var = "date"),
+                    #yAxis = 0, 
+                    name = "SMA - 200")
+  })
+  
+  output$plot2 <- renderPlot({
+    SPY_tibble_1 %>%
+      filter(date > "2020-06-01") %>% 
+      ggplot(aes(x = date)) + 
+      geom_hline(yintercept = 0, color = palette_light()[[1]]) +
+      geom_line(aes(y = macd)) +
+      geom_line(aes(y = signal), color = "blue", linetype = 2) +
+      geom_bar(aes(y = diff), stat = "identity", color = palette_light()[[1]]) +
+      labs(title = "FANG: Moving Average Convergence Divergence",
+           y = "MACD", x = "", color = "") +
+      theme_tq() +
+      scale_color_tq()
+  })
+}
+
+shinyApp(ui, server)
