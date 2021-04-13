10ca5d97c1ab63be01903c2e7b539a71b7d46146
lucas bagge <lucasbagge@lucass-MacBook-Pro.local>
2021-04-13 22:12:52 +0200

    d

---
+++ 2-semester/Stocks/Machine-Trading-Analysis-with-R.r
@@ -0,0 +1,392 @@
+######################################################
+# Machine Trading Analysis with R                    #
+# (c) Diego Fernandez Garcia 2015-2017               #
+# www.exfinsis.com                                   #
+######################################################
+
+# 1. Machine Trading Analysis Data
+
+# 1.1. Load R packages 
+library("caret")
+library("corrplot")
+library("forecast")
+library("kernlab")
+library("neuralnet")
+library("PerformanceAnalytics")
+library("quantmod")
+library("tseries")
+library("xgboost")
+
+# 1.2. Data Downloading or Reading
+
+# 1.2.1. Yahoo Finance
+# getSymbols(Symbols="SPY",src="yahoo",from="2007-01-01",to="2017-01-01")
+# spy <- SPY$SPY.Adjusted
+
+# 1.2.2. Data Reading
+data <- read.csv("Machine-Trading-Analysis-Data.txt",header=T)
+spy <- xts(data[,2],order.by=as.Date(data[,1]))
+
+# 2. Feature Creation
+
+# 2.1. Target Feature
+rspy <- dailyReturn(spy,type="log")
+
+# 2.2. Predictor Features
+rspy1 <- Lag(rspy,k=1)
+rspy2 <- Lag(rspy,k=2)
+rspy3 <- Lag(rspy,k=3)
+rspy4 <- Lag(rspy,k=4)
+rspy5 <- Lag(rspy,k=5)
+rspy6 <- Lag(rspy,k=6)
+rspy7 <- Lag(rspy,k=7)
+rspy8 <- Lag(rspy,k=8)
+rspy9 <- Lag(rspy,k=9)
+
+# 2.3. All Features
+rspyall <- cbind(rspy,rspy1,rspy2,rspy3,rspy4,rspy5,rspy6,rspy7,rspy8,rspy9)
+colnames(rspyall) <- c("rspy","rspy1","rspy2","rspy3","rspy4","rspy5","rspy6","rspy7","rspy8","rspy9")
+rspyall <- rspyall[complete.cases(rspyall),]
+
+# 3. Range Delimiting
+
+# 3.1. Training Range
+rspyt <- window(rspyall,end="2014-01-01")
+
+# 3.2. Testing Range
+rspyf <- window(rspyall,start="2014-01-01",end="2016-01-01")
+
+# 3.3. Intermediate Testing Range 
+# Same length as Training Range
+rspyp <- window(rspyall,start="2009-01-15",end="2016-01-01")
+length(rspyt)
+length(rspyp)
+
+# 3.4. Trading Range
+rspys <- window(rspyall,start="2016-01-01")
+
+# 3.5. Intermediate Trading Range
+# Same length as Training Range
+rspyi <- window(rspyall,start="2010-01-15")
+length(rspyt)
+length(rspyi)
+
+# 4. Predictor Features Selection
+
+# 4.2. Predictor Features Linear Regression
+lmta <- lm(rspy~rspy1+rspy2+rspy3+rspy4+rspy5+rspy6+rspy7+rspy8+rspy9,data=rspyt)
+summary(lmta)
+lmtb <- lm(rspy~rspy1+rspy2+rspy5,data=rspyt)
+summary(lmtb)
+# Clear Plots area before running code
+par(mfrow=c(1,2))
+plot(coredata(rspyt$rspy1),coredata(rspyt$rspy),xlab="rspy1t",ylab="rspyt")
+abline(lm(rspyt$rspy~rspyt$rspy1),col="red")
+plot(coredata(rspyt$rspy2),coredata(rspyt$rspy),xlab="rspy2t",ylab="rspyt")
+abline(lm(rspyt$rspy~rspyt$rspy2),col="red")
+plot(coredata(rspyt$rspy5),coredata(rspyt$rspy),xlab="rspy5t",ylab="rspyt")
+abline(lm(rspyt$rspy~rspyt$rspy5),col="red")
+par(mfrow=c(1,1))
+
+# 4.2. Predictor Features Correlation
+crspyt <- round(cor(rspyt[,2:10]),2)
+crspyt
+corrplot(crspyt,type="lower")
+
+# 4.3. Predictor Features Selection Filter Methods
+
+# 4.3.1. Univariate Filters
+sbfctrlt <- sbfControl(functions=lmSBF)
+sbft <- sbf(rspy~rspy1+rspy2+rspy3+rspy4+rspy5+rspy6+rspy7+rspy8+rspy9,data=rspyt,sbfControl=sbfctrlt)
+sbft
+
+# 4.4. Predictor Features Selection Wrapper Methods
+
+# 4.4.1. Recursive Feature Elimination
+rfectrlt <- rfeControl(functions=lmFuncs)
+rfet <- rfe(rspy~rspy1+rspy2+rspy3+rspy4+rspy5+rspy6+rspy7+rspy8+rspy9,data=rspyt,rfeControl=rfectrlt)
+rfet
+
+# 4.5. Predictor Features Selection Embedded Methods
+lassot <- train(rspy~rspy1+rspy2+rspy3+rspy4+rspy5+rspy6+rspy7+rspy8+rspy9,data=rspyt,method="lasso")
+predictors(lassot)
+
+# 4.6. Predictor Features Extraction
+
+# 4.6.1. Principal Component Analysis
+pcat <- princomp(rspyt[,2:10])
+summary(pcat)
+plot(pcat)
+
+# 5. Algorithm Training and Testing
+
+# 5.1. Ensemble Methods
+
+# eXtreme Gradient Boosting Machine Regression
+
+# 5.1.1. eXtreme Gradient Boosting Regression training
+xgbmta <- train(rspy~rspy1+rspy2+rspy5,data=rspyt,method="xgbTree")
+xgbmtb <- train(rspy~rspy1+rspy2+rspy3+rspy4+rspy5+rspy6+rspy7+rspy8+rspy9,data=rspyt,method="xgbTree",preProcess="pca")
+
+# eXtreme Gradient Boosting Regression optimal training parameters
+xgbmta$bestTune
+plot(xgbmta)
+xgbmtb$bestTune
+plot(xgbmtb)
+
+# eXtreme Gradient Boosting Regression training results
+xgbmta$results
+xgbmtb$results
+
+# 5.1.2. eXtreme Gradient Boosting Regression testing
+# Intermediate testing step as newdata needs to be same length as training range 
+xgbmpa <- predict.train(xgbmta,newdata=rspyp)
+xgbmpb <- predict.train(xgbmtb,newdata=rspyp)
+
+# Limited to testing range
+xgbmdfa <- cbind(index(rspyp),as.data.frame(xgbmpa))
+xgbmla <- xts(xgbmdfa[,2],order.by=as.Date(xgbmdfa[,1]))
+xgbmfa <- window(xgbmla,start="2014-01-01")
+xgbmdfb <- cbind(index(rspyp),as.data.frame(xgbmpb))
+xgbmlb <- xts(xgbmdfb[,2],order.by=as.Date(xgbmdfb[,1]))
+xgbmfb <- window(xgbmlb,start="2014-01-01")
+
+# 5.1.3. eXtreme Gradient Boosting Regression testing chart
+plot(rspyf[,1],type="l",main="eXtreme Gradient Boosting Regression A Testing Chart")
+lines(xgbmfa,col="blue")
+plot(rspyf[,1],type="l",main="eXtreme Gradient Boosting Regression B Testing Chart")
+lines(xgbmfb,col="green")
+
+# 5.1.4. eXtreme Gradient Boosting Regression forecasting accuracy
+# Convert xts to ts for accuracy function
+xgbmftsa <- ts(coredata(xgbmfa),frequency=252,start=c(2014,1))
+xgbmftsb <- ts(coredata(xgbmfb),frequency=252,start=c(2014,1))
+rspyfts <- ts(coredata(rspyf[,1]),frequency=252,start=c(2014,1))
+rspy1fts <- ts(coredata(rspyf[,2]),frequency=252,start=c(2014,1))
+accuracy(xgbmftsa,rspyfts)
+rndmape <- accuracy(rspyfts,rspy1fts)[5]
+xgbmmasea <- accuracy(xgbmftsa,rspyfts)[5]/rndmape
+xgbmmasea
+accuracy(xgbmftsb,rspyfts)
+xgbmmaseb <- accuracy(xgbmftsb,rspyfts)[5]/rndmape
+xgbmmaseb
+
+# 5.2. Algorithm Training Optimal Parameters Selection Control
+
+# 5.2.1. Time Series Cross-Validation
+tsctrlt <- trainControl(method="timeslice",initialWindow=168,horizon=82,fixedWindow=TRUE)
+
+# 5.3. Maximum Margin Methods
+
+# Support Vector Machine Regression with Radial Basis Function Kernel
+
+# 5.3.1. RBF Support Vector Machine Regression training
+rsvmta <- train(rspy~rspy1+rspy2+rspy5,data=rspyt,method="svmRadial",trControl=tsctrlt)
+rsvmtb <- train(rspy~rspy1+rspy2+rspy3+rspy4+rspy5+rspy6+rspy7+rspy8+rspy9,data=rspyt,method="svmRadial",preProcess="pca",trControl=tsctrlt)
+
+# RBF Support Vector Machine Regression optimal training parameters
+rsvmta$bestTune
+plot(rsvmta)
+rsvmtb$bestTune
+plot(rsvmtb)
+
+# RBF Support Vector Machine Regression training results
+rsvmta$results
+rsvmtb$results
+
+# 5.3.2. RBF Support Vector Machine Regression testing
+# Intermediate testing step as newdata needs to be same length as training range 
+rsvmpa <- predict.train(rsvmta,newdata=rspyp)
+rsvmpb <- predict.train(rsvmtb,newdata=rspyp)
+
+# Limited to testing range
+rsvmdfa <- cbind(index(rspyp),as.data.frame(rsvmpa))
+rsvmla <- xts(rsvmdfa[,2],order.by=as.Date(rsvmdfa[,1]))
+rsvmfa <- window(rsvmla,start="2014-01-01")
+rsvmdfb <- cbind(index(rspyp),as.data.frame(rsvmpb))
+rsvmlb <- xts(rsvmdfb[,2],order.by=as.Date(rsvmdfb[,1]))
+rsvmfb <- window(rsvmlb,start="2014-01-01")
+
+# 5.3.3. RBF Support Vector Machine Regression testing chart
+plot(rspyf[,1],type="l",main="RBF Support Vector Machine Regression A Testing Chart")
+lines(rsvmfa,col="blue")
+plot(rspyf[,1],type="l",main="RBF Support Vector Machine Regression B Testing Chart")
+lines(rsvmfb,col="green")
+
+# 5.3.4. RBF Support Vector Machine Regression forecasting accuracy
+# Convert xts to ts for accuracy function
+rsvmftsa <- ts(coredata(rsvmfa),frequency=252,start=c(2014,1))
+accuracy(rsvmftsa,rspyfts)
+rsvmmasea <- accuracy(rsvmftsa,rspyfts)[5]/rndmape
+rsvmmasea
+rsvmftsb <- ts(coredata(rsvmfb),frequency=252,start=c(2014,1))
+accuracy(rsvmftsb,rspyfts)
+rsvmmaseb <- accuracy(rsvmftsb,rspyfts)[5]/rndmape
+rsvmmaseb
+
+# 5.4. Multi-Layer Perceptron Methods
+
+# Artificial Neural Network Regression 
+
+# 5.4.1. Artificial Neural Network Regression training
+annta <- train(rspy~rspy1+rspy2+rspy5,data=rspyt,method="neuralnet",trControl=tsctrlt)
+anntb <- train(rspy~rspy1+rspy2+rspy3+rspy4+rspy5+rspy6+rspy7+rspy8+rspy9,data=rspyt,method="neuralnet",preProcess="pca",trControl=tsctrlt)
+
+# Artificial Neural Network Regression optimal training parameters
+annta$bestTune
+plot(annta)
+anntb$bestTune
+plot(anntb)
+
+# Artificial Neural Network Regression training results
+annta$results
+anntb$results
+
+# 5.4.2. Artificial Neural Network Regression testing
+# Intermediate testing step as newdata needs to be same length as training range 
+annpa <- predict.train(annta,newdata=rspyp)
+annpb <- predict.train(anntb,newdata=rspyp)
+
+# Limited to testing range
+anndfa <- cbind(index(rspyp),as.data.frame(annpa))
+annla <- xts(anndfa[,2],order.by=as.Date(anndfa[,1]))
+annfa <- window(annla,start="2014-01-01")
+anndfb <- cbind(index(rspyp),as.data.frame(annpb))
+annlb <- xts(anndfb[,2],order.by=as.Date(anndfb[,1]))
+annfb <- window(annlb,start="2014-01-01")
+
+# 5.4.3. Artificial Neural Network Regression testing chart
+plot(rspyf[,1],type="l",main="Artificial Neural Network Regression A Testing Chart")
+lines(annfa,col="blue")
+plot(rspyf[,1],type="l",main="Artificial Neural Network Regression B Testing Chart")
+lines(annfb,col="green")
+
+# 5.4.4. Artificial Neural Network Regression forecasting accuracy
+# Convert xts to ts for accuracy function
+annftsa <- ts(coredata(annfa),frequency=252,start=c(2014,1))
+accuracy(annftsa,rspyfts)
+annmasea <- accuracy(annftsa,rspyfts)[5]/rndmape
+annmasea
+annftsb <- ts(coredata(annfb),frequency=252,start=c(2014,1))
+accuracy(annftsb,rspyfts)
+annmaseb <- accuracy(annftsb,rspyfts)[5]/rndmape
+annmaseb
+
+# 5.6. Algorithm Testing Accuracy Comparison
+accuracy(xgbmftsb,rspyfts)
+accuracy(rsvmftsa,rspyfts)
+accuracy(annftsa,rspyfts)
+xgbmmaseb
+rsvmmasea
+annmasea
+
+# 6. Machine Trading Strategies
+
+# 6.1. Ensemble Method Trading Strategy
+
+# 6.1.1. eXtreme Gradient Boosting Regression forecasting
+# Intermediate forecasting step as newdata needs to be same length as training range 
+xgbmi <- predict.train(xgbmtb,newdata=rspyi)
+
+# Limited to trading range
+xgbmdfi <- cbind(index(rspyi),as.data.frame(xgbmi))
+xgbmli <- xts(xgbmdfi[,2],order.by=as.Date(xgbmdfi[,1]))
+xgbms <- window(xgbmli,start="2016-01-01")
+
+# 6.1.2. eXtreme Gradient Boosting Regression trading signals
+xgbmsig <- Lag(ifelse(Lag(xgbms)<0&xgbms>0,1,ifelse(Lag(xgbms)>0&xgbms<0,-1,0)))
+xgbmsig[is.na(xgbmsig)] <- 0
+
+# 6.1.3. eXtreme Gradient Boosting Regression trading positions
+xgbmpos <- ifelse(xgbmsig>1,1,0)
+for(i in 1:length(xgbmpos)){xgbmpos[i] <- ifelse(xgbmsig[i]==1,1,ifelse(xgbmsig[i]==-1,0,xgbmpos[i-1]))}
+xgbmpos[is.na(xgbmpos)] <- 0
+xgbmtr <- cbind(xgbms,xgbmsig,xgbmpos)
+colnames(xgbmtr) <- c("xgbms","xgbmsig","xgbmpos")
+View(xgbmtr)
+
+# 6.2. Maximum Margin Method Trading Strategy
+
+# 6.2.1. RBF Support Vector Machine Regression forecasting
+# Intermediate forecasting step as newdata needs to be same length as training range 
+rsvmi <- predict.train(rsvmta,newdata=rspyi)
+
+# Limited to trading range
+rsvmdfi <- cbind(index(rspyi),as.data.frame(rsvmi))
+rsvmli <- xts(rsvmdfi[,2],order.by=as.Date(rsvmdfi[,1]))
+rsvms <- window(rsvmli,start="2016-01-01")
+
+# 6.2.2. RBF Support Vector Machine Regression trading signals
+rsvmsig <- Lag(ifelse(Lag(rsvms)<0&rsvms>0,1,ifelse(Lag(rsvms)>0&rsvms<0,-1,0)))
+rsvmsig[is.na(rsvmsig)] <- 0
+
+# 6.2.3. RBF Support Vector Machine Regression trading positions
+rsvmpos <- ifelse(rsvmsig>1,1,0)
+for(i in 1:length(rsvmpos)){rsvmpos[i] <- ifelse(rsvmsig[i]==1,1,ifelse(rsvmsig[i]==-1,0,rsvmpos[i-1]))}
+rsvmpos[is.na(rsvmpos)] <- 0
+rsvmtr <- cbind(rsvms,rsvmsig,rsvmpos)
+colnames(rsvmtr) <- c("rsvms","rsvmsig","rsvmpos")
+View(rsvmtr)
+
+# 6.3. Multi-Layer Perceptron Method Trading Strategy
+
+# 6.3.1. Artificial Neural Network Regression forecasting
+# Intermediate forecasting step as newdata needs to be same length as training range 
+anni <- predict.train(annta,newdata=rspyi)
+
+# Limited to trading range
+anndfi <- cbind(index(rspyi),as.data.frame(anni))
+annli <- xts(anndfi[,2],order.by=as.Date(anndfi[,1]))
+anns <- window(annli,start="2016-01-01")
+
+# 6.3.2. Artificial Neural Network Regression trading signals
+annsig <- Lag(ifelse(Lag(anns)<0&anns>0,1,ifelse(Lag(anns)>0&anns<0,-1,0)))
+annsig[is.na(annsig)] <- 0
+
+# 6.3.3. Artificial Neural Network Regression trading positions
+annpos <- ifelse(annsig>1,1,0)
+for(i in 1:length(annpos)){annpos[i] <- ifelse(annsig[i]==1,1,ifelse(annsig[i]==-1,0,annpos[i-1]))}
+annpos[is.na(annpos)] <- 0
+anntr <- cbind(anns,annsig,annpos)
+colnames(anntr) <- c("anns","annsig","annpos")
+View(anntr)
+
+# 7. Machine Trading Strategies Performance Comparison
+
+# 7.1. Ensemble Method Trading Strategy Performance Comparison
+xgbmret <- xgbmpos*rspys[,1]
+xgbmretc <- ifelse((xgbmsig==1|xgbmsig==-1)&xgbmpos!=Lag(xgbmpos),(xgbmpos*rspys[,1])-0.001,xgbmpos*rspys[,1])
+xgbmcomp <- cbind(xgbmret,xgbmretc,rspys[,1])
+colnames(xgbmcomp) <- c("xgbmret","xgbmretc","rspy")
+table.AnnualizedReturns(xgbmcomp)
+charts.PerformanceSummary(xgbmcomp)
+
+# 7.2. Maximum Margin Method Trading Strategy Performance Comparison
+rsvmret <- rsvmpos*rspys[,1]
+rsvmretc <- ifelse((rsvmsig==1|rsvmsig==-1)&rsvmpos!=Lag(rsvmpos),(rsvmpos*rspys[,1])-0.001,rsvmpos*rspys[,1])
+rsvmcomp <- cbind(rsvmret,rsvmretc,rspys[,1])
+colnames(rsvmcomp) <- c("rsvmret","rsvmretc","rspy")
+table.AnnualizedReturns(rsvmcomp)
+charts.PerformanceSummary(rsvmcomp)
+
+# 7.3. Multi-Layer Perceptron Method Trading Strategy Performance Comparison
+annret <- annpos*rspys[,1]
+annretc <- ifelse((annsig==1|annsig==-1)&annpos!=Lag(annpos),(annpos*rspys[,1])-0.001,annpos*rspys[,1])
+anncomp <- cbind(annret,annretc,rspys[,1])
+colnames(anncomp) <- c("annret","annretc","rspy")
+table.AnnualizedReturns(anncomp)
+charts.PerformanceSummary(anncomp)
+
+# 7.4. Machine Trading Strategies Performance Comparison
+
+# 7.4.1. Without Trading Commissions
+comp <- cbind(xgbmret,rsvmret,annret,rspys[,1])
+colnames(comp) <- c("xgbmret","rsvmret","annret","rspy")
+table.AnnualizedReturns(comp)
+charts.PerformanceSummary(comp)
+
+# 7.4.2. With Trading Commissions
+compc <- cbind(xgbmretc,rsvmretc,annretc,rspys[,1])
+colnames(compc) <- c("xgbmretc","rsvmretc","annretc","rspy")
+table.AnnualizedReturns(compc)
+charts.PerformanceSummary(compc)
---
+++ 2-semester/ip/TLsningerUge9.py
@@ -0,0 +1,230 @@
+#Exercise 15.1 (decorator returns int)
+def enforce_integer_return(f):
+    def wrapper(*args, **kwargs):
+        result = f(*args, **kwargs)
+        assert isinstance(result, int), "result must be int"
+        return result
+    return wrapper
+
+@enforce_integer_return
+def my_sum(x, y):
+    return x + y
+
+print(my_sum(19,23))
+print(my_sum(x=13,y=56))
+print(my_sum(287.84, 132.85))
+
+#Exercise 15.2 (box it)
+def BoxIt(f):
+    def wrapper(*args, **kwargs):
+        result = str(f(*args, **kwargs))
+        line = "-" * (len( result) + 4)
+        return f"{line}\n| { result} |\n{line}"
+    return wrapper
+
+@BoxIt
+def plus(x, y):
+    return "The sum of %s and %s is %s" % (x, y, x + y)
+
+
+#Exercise 15.3 (multiline box it) [optional]
+def multi_BoxIt(f):
+    def wrapper(*args, **kwargs):
+        result = str(f(*args, **kwargs))
+        lines = result.split("\n")
+        width = len(max(lines, key=len))
+        lines = [f"| {line:<10} |\n" for line in lines]
+        headerline = "+" + "-" * (width + 2) + "+"
+        return headerline + "\n" + "".join(lines) + headerline
+    return wrapper
+
+@multi_BoxIt
+@multi_BoxIt
+@multi_BoxIt
+def test():
+    return "This\n   is a\n      test"
+
+
+#Exercise 15.4** (decorator composition) [optional]
+def compose(*decorators):
+    def wrapper(f):
+        for dec in reversed(decorators):
+            f = dec(f)
+        return f
+    return wrapper
+
+
+@compose(multi_BoxIt, multi_BoxIt, multi_BoxIt,multi_BoxIt, multi_BoxIt, multi_BoxIt)
+def plus(x, y):
+    return "The sum of %s and %s is %s" % (x, y, x + y)
+
+
+
+#Exercise 16.1 (coins)
+def memoize(f):
+    # answers[args] = f(*args)
+    answers = {}
+    def wrapper(*args):
+        if args not in answers:
+            answers[args] = f(*args)
+        return answers[args]
+    return wrapper
+
+def trace(f):  # decorator to trace recursive calls
+    indent = 0
+    def wrapper(*args):
+        nonlocal indent
+        spaces = '|  ' * indent
+        arg_str = ', '.join(map(repr, args))
+        print(spaces + f'{f.__name__}({arg_str})')
+        indent += 1
+        result = f(*args)
+        indent -= 1
+        print(spaces + f'> {result}')
+        return result
+    return wrapper
+
+
+# a.
+def change_greedy(value, coins):
+    '''
+    Implement a function change_greedy(value, coins) that implements the greedy change strategy,
+    and returns a list of integers, each integer in coins, and with sum equal to value.
+    Example: change_greedy(35, (1, 7, 10)) should return [10, 10, 10, 1, 1, 1, 1, 1].
+    '''
+    coins = sorted(coins, reverse=True)
+    answer = []
+    for coin in coins:
+        while value >= coin:
+            answer.append(coin)
+            value -= coin
+    return answer if value == 0 else None
+
+
+# b.
+@memoize
+def number_of_coins(value, coins):
+    '''
+    Implement a recursive function number_of_coins(value, coins)
+    that returns the numer of coins in an optimal solution.
+    '''
+    if value == 0:
+        return 0
+    return min([1 + number_of_coins(value - coin, coins) for coin in coins if
+                coin <= value])
+
+
+# c.
+def change(value, coins):
+    '''
+    Implement a recursive function change(value, coins) that returns an optimal solution,
+    i.e. a list of integers, each integer in coins and where the sum equals value.
+    '''
+
+    @memoize
+    @trace
+    def solve(x):
+        if x == 0:  # base case
+            return []
+        else:
+            answer = None
+            for coin in coins:
+                if coin <= x:
+                    solution = solve(x - coin)
+                    if (solution != None and (
+                            answer == None or len(solution) + 1 < len(answer))):
+                        answer = solution + [coin]
+            return answer
+
+    return sorted(solve(value))
+
+
+# d.
+def change_iterative(value, coins):
+    '''
+    Implement a function change_iterative(value, coins) that returns an optimal solution.
+    The function should fill out a table of solutions for increasing values of value.
+    '''
+    solutions = [None] * (value + 1)
+    for x in range(value + 1):
+        if x == 0:
+            answer = []
+        else:
+            answer = None
+            for coin in coins:
+                if coin <= x:
+                    solution = solutions[x - coin]
+                    if (solution != None and
+                            (answer == None or len(solution) + 1 < len(
+                                answer))):
+                        answer = solution + [coin]
+        solutions[x] = answer
+
+    return solutions[value]
+
+
+@memoize
+def change_andreas(value, coins):
+    if value == 0:
+        return []
+    else:
+        return min(
+            [[coin] + change_andreas(value - coin, coins) for coin in coins if
+             coin <= value], key=len)
+
+
+#test
+def exchange(value, coins):
+    print(f"{value=} {coins=}")
+    print(f"  Number of coins: {number_of_coins(value, coins)}")
+    print(f"  Greedy: {change_greedy(value, coins)}")
+    print(f"  Optimal: {change(value, coins)}")
+    print(f"  Iterative: {change_iterative(value, coins)}")
+    print(f"  Andreas: {change_andreas(value, coins)}")
+
+exchange(35, (10, 7, 1))
+exchange(33, (1, 2, 5, 10, 20))
+exchange(21, (1, 3, 10, 13))
+exchange(14, (1, 7, 10))
+exchange(157, (1, 2, 5, 10, 20, 50, 100, 200))
+
+
+
+#Exercise 16.2 (missing spaces) [optional]
+
+text = "aduckcrossestheroad"
+words = { 'a', 'ad', 'cross', 'duck', 'est', 'he', 'road', 'the' }
+
+# text = text[:250]
+
+def greedy(text, words):
+    n = len(text)
+    max_len = max([len(w) for w in words])
+    answer = []
+    i = 0
+    while i < n:
+        for m in range(1, min(n-i+1, max_len+1))[::-1]:
+            if text[i:i+m] in words:
+                break
+        answer.append(text[i:i+m])
+        i += m
+
+    return answer
+
+def solve(text, strings):
+    n = len(text)
+    answers = [None] * (n + 1)
+    for i in range(n+1):
+        if i == 0:
+            answer = []
+        else:
+            answer = answers[i - 1] + [text[i-1]]
+            for w in words:
+                m = len(w)
+                if i >= m and text[i-m:i] == w:
+                    if len(answer) > 1 + len(answers[i - m]):
+                        answer = answers[i - m] + [w]
+
+        answers[i] = answer
+
+    return answers[n]
