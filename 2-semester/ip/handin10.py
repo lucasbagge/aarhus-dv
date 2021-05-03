fcf76f511a99c37112b3e215c6841aa60508a84d
lucas bagge <lucasbagge@lucass-MacBook-Pro.local>
2021-05-03 09:29:57 +0200

    handin10

--- 2-semester/ip/handin10.py
+++ 2-semester/ip/handin10.py
@@ -14,5 +14,45 @@ Hint. import matplotlib.pyplot as plt; plt.imshow([[1,2,4],[0,3,-1]]); plt.color
 Hint. imshow takes an optional argument extent to adjust the x-axis and y-axis.
 Hint. Use itertools.islice to get a finite prefix of an infinite iterator. 
 Depending of your approach, collections.Counter might become handy.
+"""
 
 """
+  Modules
+"""
+import random as rd
+import itertools as it
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+  a)
+"""
+def random_walk():
+  x = y = 0
+  offsets = [ (0, 1), (0, -1), (1,0), (-1, 0) ]
+  while True:
+    yield (x,y)
+    dx, dy = rd.choice(offsets)
+    x, y = x + dx, y + dy
+
+print(*it.islice(random_walk(), 15))
+
+"""
+b)
+"""
+# Random walk på længden 1000
+walk = list(it.islice(random_walk(), 0, 1000))
+
+xy = list(zip(*walk))
+
+x = list(xy[0])
+y = list(xy[1])
+# sætter bins til at have en afstand på 1 enhed. 
+heatmap, xedges, yedges = np.histogram2d(x, y, bins=[np.arange(min(x),max(x),1),np.arange(min(y),max(y),1)])
+extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+plt.imshow(heatmap, origin='lower', extent=extent)
+plt.colorbar()
+plt.show()
+
+# https://stackoverflow.com/questions/61387570/create-infinite-random-walk-using-iterators-in-python
+# https://stackoverflow.com/questions/61631246/python-plot-frequency-of-negative-x-y-coordinates
